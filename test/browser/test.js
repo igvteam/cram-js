7a146df6726739ae6429e28433fa7fd8a8cb0647
Jim Robinson <jrobinso@broadinstitute.org>
2018-11-22 20:18:31 -0800

    update browser test with more output

--- test/browser/test.js
+++ test/browser/test.js
@@ -1,5 +1,6 @@
 const IndexedCramFile = gmodCRAM.IndexedCramFile;
 const CraiIndex = gmodCRAM.CraiIndex;
+const CramFile = gmodCRAM.CramFile;
 
 const indexedFile2 = new IndexedCramFile({
 
@@ -9,7 +10,7 @@ const indexedFile2 = new IndexedCramFile({
     url: '../data/ce_5.tmp.cram.crai',
   }),
 
-  seqFetch:  (seqId, start, end) => {
+  seqFetch: (seqId, start, end) => {
     let fakeSeq = ''
     for (let i = start; i <= end; i += 1) {
       fakeSeq += 'A'
@@ -30,12 +31,25 @@ indexedFile2.getRecordsForRange(0, 10000, 20000)
 
     records.forEach(record => {
 
-      output += 'got a record named' + record.readName + '<p/>'
+      output += 'got a record named ' + record.readName + '<p/>'
 
-      record.readFeatures.forEach(({code, pos, refPos, ref, sub}) => {
+      output += 'read bases: ' + record.getReadBases() + '<br/>';
 
-        if (code === 'X') {
-          output += record.readName + ' shows a base substitution of ' + ref + '->' + sub + ' at ' + refPos + '<br/>';
+      output += 'quality scores: ' + (record.isPreservingQualityScores() ? record.qualityScores.join(',') : 'none') + '<br/>';
+
+      output += 'read features:' + '<br/>';
+
+      record.readFeatures.forEach(({code, data, pos, refPos, ref, sub}) => {
+
+        output += 'code: ' + code + ' ';
+
+        switch(code) {
+          case 'X':
+            output += 'base substitution of ' + ref + '->' + sub + ' at ' + refPos + '<br/>';
+            break;
+          case 'D':
+            output += 'deletion of ' + data + ' bases at '+ refPos + '<br/>';
+            break;
         }
 
       })
@@ -44,3 +58,24 @@ indexedFile2.getRecordsForRange(0, 10000, 20000)
     document.getElementById('output').innerHTML = output;
 
   })
+
+
+const cramFile = new CramFile({url: '../data/ce_5.tmp.cram'});
+
+cramFile.getSamHeader()
+  .then(function (header) {
+    // Find the SQ lines
+
+    let sqtext = ''
+    for(let line of header) {
+
+      if('SQ' === line.tag) {
+        sqtext += line.data[0].value;
+        sqtext += "<br/>"
+      }
+    }
+
+    document.getElementById('header').innerHTML = sqtext;
+  });
+
+
